3207dd53eb49e29ae739a46dcec509c6c960f4c9
Woojin <wj05132@gmail.com>
2024-07-30 11:03:38 +0900

    install redis

--- chatcat/settings.py
+++ chatcat/settings.py
@@ -1,11 +1,7 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 LOGIN_URL = '/login/'  # Redirects unauthenticated users to the login page
 
